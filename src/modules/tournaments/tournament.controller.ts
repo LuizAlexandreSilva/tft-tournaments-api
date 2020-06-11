25188bbce772dc65705861ae4ddbfaa51b7d078e
LuizAlexandreSilva <alexandrehsilva96@gmail.com>
2020-06-10 23:09:41 -0400

    feature: create tournament with multiple organizers

--- src/modules/tournaments/tournament.controller.ts
+++ src/modules/tournaments/tournament.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { TournamentService } from './tournament.service';
 import { Tournament } from './entities/tournament.entity';
 import { CreateTournamentDTO } from './dtos/CreateTournamentDTO';
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
 
 @Controller('/tournaments')
 export class TournamentController {
   constructor(private readonly tournamentService: TournamentService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Post('/create')
   async create(
     @Body() createTournamentDTO: CreateTournamentDTO
